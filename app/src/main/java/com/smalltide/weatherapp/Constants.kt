bc7b0c46e602829e67e4098669e170aa0d53ad21
varun <varunpandey2001@gmail.com>
2022-10-10 13:54:29 +0530

    WeatherApp

---
+++ app/src/main/java/com/smalltide/weatherapp/Constants.kt
@@ -0,0 +1,40 @@
+package com.smalltide.weatherapp
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+object Constants{
+
+    const val APP_ID: String ="47b984de47701a4d2d483fe0523bdfb1"
+    const val BASE_URL: String = "https://api.openweathermap.org/data/"
+    const val METRIC_UNIT: String = "metric"
+
+    // TODO (STEP 3: Add the SharedPreferences name and key name for storing the response data in it.)
+    // START
+    const val PREFERENCE_NAME = "WeatherAppPreference"
+    const val WEATHER_RESPONSE_DATA = "weather_response_data"
+    // END
+
+    fun isNetworkAvailable(context: Context) : Boolean {
+        // It answers the queries about the state of network connectivity.
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network      = connectivityManager.activeNetwork ?: return false
+            val activeNetWork = connectivityManager.getNetworkCapabilities(network) ?: return false
+            return when {
+                activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                //for other device how are able to connect with Ethernet
+                activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            // Returns details about the currently active default data network.
+            val networkInfo = connectivityManager.activeNetworkInfo
+            return networkInfo != null && networkInfo.isConnectedOrConnecting
+        }
+    }
+
+}
